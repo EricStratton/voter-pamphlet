f361d9acefb9d2d2c49a24b4ee46ca000584c275
Eric Stratton <strattonericj@gmail.com>
2020-10-22 11:04:13 -0700

    make age input prompt toggle away after user enters their age
    Co-authored-by: Riley McAdoo <riley.mcadoo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
-$(document).ready(function () {
-  const age = parseInt(prompt("How old are you?"));
+// $(document).ready(function () {
+//   const age = parseInt(prompt("How old are you?"));
 
-  if (age >= 18) {
-    $('#voterInfo').show();
-  } else {
-    $('#under-18').show();
-  }
+//   if (age >= 18) {
+//     $('#voterInfo').show();
+//   } else {
+//     $('#under-18').show();
+//   }
+// });
+
+$(document).ready(function () {
+  $("form#agePrompt").submit(function (event) {
+    event.preventDefault();
+    const ageInput = parseInt($("#agePromptInput").val());
+    $("#agePrompt").toggle();
+
+    if (ageInput >= 18) {
+      $('#voterInfo').show();
+    } else {
+      $('#under-18').show();
+    }
+  });
 });
